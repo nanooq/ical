4cb8bd83b99ba12f1557d818d602d431a79f69a1
nanooq <gitlab.com@nanooq.org>
2019-04-08 14:11:38 +0200

    refactor code

--- ical.py
+++ ical.py
@@ -60,9 +60,9 @@ longdesc_markdown_format = """\
 calendars = {}
 
 # figure out the day start in local time
-now = datetime.datetime.now(dateutil.tz.tzlocal())
-now = now.replace(hour=0, minute=0, second=0, microsecond=0)
-now = now.astimezone(dateutil.tz.tzutc())
+now = datetime.datetime.now( dateutil.tz.tzlocal() )
+now = now.replace( hour=0, minute=0, second=0, microsecond=0 )
+now = now.astimezone( dateutil.tz.tzutc() )
 
 
 def simple_tzinfos(abbrev, offset):
@@ -71,78 +71,78 @@ def simple_tzinfos(abbrev, offset):
     elif abbrev == "UTC" and offset == 0:
         return dateutil.tz.tzutc()
     else:
-        print("simple_tzinfos:", abbrev, offset)
+        print( "simple_tzinfos:", abbrev, offset )
     return 0
 
 
-class FmtString(str):
+class FmtString( str ):
     def __format__(self, format_spec):
         if not self or not format_spec:
             return self
 
         if format_spec[:5] == "html:":
-            return format_spec[5:] % html.escape(self)
+            return format_spec[5:] % html.escape( self )
         elif format_spec[:3] == "md:":
-            return format_spec[3:] % markdown.markdown(self, safe_mode="escape")
+            return format_spec[3:] % markdown.markdown( self, safe_mode="escape" )
         else:
             return format_spec % self
 
 
-class Event(dict):
+class Event( dict ):
     def __init__(self, entries):
         for k, v in entries.items():
             self[k] = v
         self.upd = None
 
     def set_update_events(self, updates):
-        if len(updates) > 0:
+        if len( updates ) > 0:
             self.upd = updates[0]
-            self.upd.set_update_events(updates[1:])
+            self.upd.set_update_events( updates[1:] )
 
     def get_ical(self, filter=None):
         pass
 
     def __getitem__(self, key):
-        val = super(Event, self).get(key, None)
+        val = super( Event, self ).get( key, None )
         if val == None:
             tim, evt = self.get_time()[0]
             if key == 'datetime':
                 val = tim
             elif key == 'uid':
-                val = FmtString(evt["UID"])
+                val = FmtString( evt["UID"] )
             elif key == 'summary':
-                val = FmtString(evt["SUMMARY"])
+                val = FmtString( evt["SUMMARY"] )
             elif key == 'description':
-                val = FmtString(evt["DESCRIPTION"])
+                val = FmtString( evt["DESCRIPTION"] )
             elif key == 'location':
-                val = FmtString(evt["LOCATION"])
+                val = FmtString( evt["LOCATION"] )
             elif key == 'image':
-                val = FmtString("")
+                val = FmtString( "" )
             elif key == 'follow_ups':
                 val = ""
                 pending = self.get_time()[1:]
                 if pending:
-                    val = ", ".join([p[0].strftime("%d. %m. %Y")
-                                     for p in pending[:3]]) + [".", "…"][len(pending) > 3]
-                val = FmtString(val)
+                    val = ", ".join( [p[0].strftime( "%d. %m. %Y" )
+                                      for p in pending[:3]] ) + [".", "…"][len( pending ) > 3]
+                val = FmtString( val )
 
         return val
 
     def __setitem__(self, key, value):
         rdict = {'n': "\n"}
-        value = re.sub("\\\\(.)",
-                       lambda x: rdict.get(x.group(1), x.group(1)), value)
-        super(Event, self).__setitem__(key, value)
+        value = re.sub( "\\\\(.)",
+                        lambda x: rdict.get( x.group( 1 ), x.group( 1 ) ), value )
+        super( Event, self ).__setitem__( key, value )
 
     def __lt__(self, other):
         owntimes = self.get_time()
         othertimes = other.get_time()
 
-        if len(owntimes) > 0 and len(othertimes) > 0:
+        if len( owntimes ) > 0 and len( othertimes ) > 0:
             return self.get_time()[0][0] < other.get_time()[0][0]
-        elif len(othertimes) > 0:
+        elif len( othertimes ) > 0:
             return True
-        elif len(owntimes) > 0:
+        elif len( owntimes ) > 0:
             return False
         else:
             return False
@@ -150,57 +150,57 @@ class Event(dict):
     def is_pending(self):
         owntimes = self.get_time()
 
-        if len(owntimes):
+        if len( owntimes ):
             return now < self.get_time()[0][0]
         else:
             return False
 
     def get_time(self, times=[]):
         if "RECURRENCE-ID" in self:
-            rec = dateutil.parser.parse(self["RECURRENCE-ID"],
-                                        tzinfos=simple_tzinfos)
+            rec = dateutil.parser.parse( self["RECURRENCE-ID"],
+                                         tzinfos=simple_tzinfos )
             times = [t for t in times if t[0] != rec]
 
         if "DTSTART" in self and "RRULE" in self:
-            rr = dateutil.rrule.rrulestr(self.rrtext, tzinfos=simple_tzinfos)
-            pending = rr.between(now, now + datetime.timedelta(120))
-            times = times + [(p.astimezone(dateutil.tz.tzlocal()), self)
+            rr = dateutil.rrule.rrulestr( self.rrtext, tzinfos=simple_tzinfos )
+            pending = rr.between( now, now + datetime.timedelta( 120 ) )
+            times = times + [(p.astimezone( dateutil.tz.tzlocal() ), self)
                              for p in pending]
 
         elif "DTSTART" in self:
-            dts = dateutil.parser.parse(self["DTSTART"], tzinfos=simple_tzinfos)
-            times = times + [(dts.astimezone(dateutil.tz.tzlocal()), self)]
+            dts = dateutil.parser.parse( self["DTSTART"], tzinfos=simple_tzinfos )
+            times = times + [(dts.astimezone( dateutil.tz.tzlocal() ), self)]
 
         else:
-            times = times + [(now.astimezone(dateutil.tz.tzlocal()), self)]
+            times = times + [(now.astimezone( dateutil.tz.tzlocal() ), self)]
 
         if self.upd:
-            times = self.upd.get_time(times)
+            times = self.upd.get_time( times )
 
         times.sort()
-        while len(times) > 1 and times[0][0] < now:
+        while len( times ) > 1 and times[0][0] < now:
             times = times[1:]
 
         return times
 
 
-class Calendar(object):
+class Calendar( object ):
     def __init__(self, url=None):
         if not url:
             url = default_url
 
         self.url = url
 
-        data = urllib.request.urlopen(self.url).read()
-        data = data.decode('utf-8')
+        data = urllib.request.urlopen( self.url ).read()
+        data = data.decode( 'utf-8' )
 
         # normalize line endings
-        data = data.replace("\r\n", "\n")
-        data = data.replace("\n\r", "\n")
+        data = data.replace( "\r\n", "\n" )
+        data = data.replace( "\n\r", "\n" )
         # ical continuation lines
-        data = data.replace("\n ", "")
+        data = data.replace( "\n ", "" )
 
-        lines = [l.strip() for l in data.split("\n")]
+        lines = [l.strip() for l in data.split( "\n" )]
 
         self.eventdict = {}
         cur_event = None
@@ -212,10 +212,10 @@ class Calendar(object):
                 continue
 
             extra = None
-            key, value = l.split(":", 1)
+            key, value = l.split( ":", 1 )
 
             if ";" in key:
-                key, extra = key.split(";", 1)
+                key, extra = key.split( ";", 1 )
 
             if key == "BEGIN" and value == "VEVENT":
                 cur_event = {}
@@ -233,7 +233,7 @@ class Calendar(object):
                 uid = cur_event["UID"]
                 if uid not in self.eventdict:
                     self.eventdict[uid] = []
-                self.eventdict[uid].append(Event(cur_event))
+                self.eventdict[uid].append( Event( cur_event ) )
                 self.eventdict[uid][-1].rrtext = raw_rrtext
                 cur_event = None
                 continue
@@ -246,9 +246,9 @@ class Calendar(object):
         self.eventlist = []
 
         for id, ev in self.eventdict.items():
-            ev.sort( key=lambda x: int(x.get("SEQUENCE", "0")))
-            ev[0].set_update_events(ev[1:])
-            self.eventlist.append(ev[0])
+            ev.sort( key=lambda x: int( x.get( "SEQUENCE", "0" ) ) )
+            ev[0].set_update_events( ev[1:] )
+            self.eventlist.append( ev[0] )
 
         self.eventlist.sort()
 
@@ -256,51 +256,58 @@ class Calendar(object):
         el = [e for e in self.eventlist if e.is_pending()]
         if limit > 0:
             el = el[:limit]
-        text = "\n".join([template.format_map(e) for e in el])
+        text = "\n".join( [template.format_map( e ) for e in el] )
         return text
 
 
-def ical_replace(m):
-    args = m.group(2).split()
+def ical_replace( m ):
+    # TODO: What is m?
+    args = m.group( 2 ).split()
     format = "summary"
     limit = -1
     url = default_url
 
-    if len(args) >= 1:
+    if len( args ) >= 1:
         if ":" in args[0]:
-            format, limit = args[0].split(":", 2)
-            limit = int(limit)
+            format, limit = args[0].split( ":", 2 )
+            limit = int( limit )
         else:
             format = args[0]
 
-    if len(args) >= 2:
+    if len( args ) >= 2:
         url = args[1]
 
     if not url in calendars:
-        calendars[url] = Calendar(url)
+        calendars[url] = Calendar( url )
 
     if format == "full":
-        txtdata = calendars[url].get_formatted(longdesc_markdown_format, limit)
+        txtdata = calendars[url].get_formatted( longdesc_markdown_format, limit )
     else:
-        txtdata = calendars[url].get_formatted(shortdesc_markdown_format, limit)
+        txtdata = calendars[url].get_formatted( shortdesc_markdown_format, limit )
 
-    txtdata = markdown.markdown(txtdata)
+    txtdata = markdown.markdown( txtdata )
 
-    return m.group(1) + txtdata + m.group(3)
+    return m.group( 1 ) + txtdata + m.group( 3 )
 
 
 if __name__ == '__main__':
     if not sys.argv[1:]:
         calendar = Calendar()
-        # print (markdown.markdown (c.get_formatted (shortdesc_markdown_format)))
-        print(calendar.get_formatted(hasi_format))
+        print( calendar.get_formatted( hasi_format ) )
 
-    for f in sys.argv[1:]:
-        data = open(f).read()
-        data2 = re.sub(r'(?ims)(<!--\s*ical\b\s*(.*?)\s*-->).*?(<!--\s*/ical\s*-->)',
-                       ical_replace, data)
+    for filename in sys.argv[1:]:
+        html_page_of_last_replacement = open( filename ).read()
+        html_page_for_current_replacement = re.sub(
+            # (?ims): re.IGNORECASE, re.MULTILINE, re.DOTALL
+            # \s*: 0...n unicode whitespaces
+            # \b: empty string bound of a word
+            # (.*?): capture group of the minimal characters found possible
+            # TODO .*?: not really sure
+            r'(?ims)(<!--\s*ical\b\s*(.*?)\s*-->).*?(<!--\s*/ical\s*-->)',
+            ical_replace,
+            html_page_of_last_replacement )
 
-        if data2 != data:
-            outf = open(f, "w")
-            outf.write(data2)
+        if html_page_for_current_replacement != html_page_of_last_replacement:
+            outf = open( filename, "w" )
+            outf.write( html_page_for_current_replacement )
             outf.close()
